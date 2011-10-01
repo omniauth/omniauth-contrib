f6f3e44baed07743d55d1e74bfefa1ce617a245a
Michael Bleigh <michael@intridea.com>
2011-10-01 01:48:09 -0500

    Adds GitHub strategy and OAuth2 dependency.

--- examples/sinatra.rb
+++ examples/sinatra.rb
@@ -10,17 +10,19 @@ use Rack::Session::Cookie
 
 use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
 
 get '/' do
   <<-HTML
   <ul>
     <li><a href='/auth/twitter'>Sign in with Twitter</a></li>
+    <li><a href='/auth/github'>Sign in with GitHub</a></li>
   </ul>
   HTML
 end
 
 get '/auth/:provider/callback' do
   content_type 'text/plain'
-  request.env['omniauth.auth'].inspect
+  request.env['omniauth.auth'].info.to_hash.inspect
 end
--- lib/omniauth-contrib.rb
+++ lib/omniauth-contrib.rb
@@ -3,5 +3,8 @@ require "omniauth-contrib/version"
 module OmniAuth
   module Strategies
     autoload :Twitter, 'omniauth/strategies/twitter'
+    autoload :GitHub,  'omniauth/strategies/github'
   end
 end
+
+OmniAuth.config.add_camelization 'github', 'GitHub'
