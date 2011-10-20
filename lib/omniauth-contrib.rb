88b7e9bd973c3cec7190747e60f54fd95dd7de12
Michael Bleigh <michael@intridea.com>
2011-10-19 23:02:50 -0400

    Update.

--- lib/omniauth-contrib.rb
+++ lib/omniauth-contrib.rb
@@ -5,7 +5,10 @@ module OmniAuth
   module Strategies
     autoload :Twitter,  'omniauth/strategies/twitter'
     autoload :GitHub,   'omniauth/strategies/github'
+    autoload :OpenID,   'omniauth/strategies/openid'
   end
 end
 
 OmniAuth.config.add_camelization 'github', 'GitHub'
+OmniAuth.config.add_camelization 'openid', 'OpenID'
+OmniAuth.config.add_camelization 'open_id', 'OpenID'
